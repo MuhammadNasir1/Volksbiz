c6933caff774adefbfdda01cad87fdcd3b706f48
MuhammadNasir1 <nmian7080@gmail.com>
2024-06-10 17:49:38 +0500

    change path name

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('login', [authController::class, 'login']);
 Route::post('register', [authController::class, 'register']);
 Route::post('/changepasword', [authController::class, 'changepasword']);
-Route::post('/updateSettings', [authController::class, 'updateSettings']);
+Route::post('/updateProfile', [authController::class, 'updat-eSettings']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [authController::class, 'logout']);
     Route::get('/getUserProfile', [authController::class, 'getUserProfile']);
