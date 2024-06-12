d0fadbbf9e6800d08ecc0dcdb39c7613de057222
MuhammadNasir1 <nmian7080@gmail.com>
2024-06-12 16:46:14 +0500

    add mig

---
+++ app/Models/Blogs.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blogs extends Model
+{
+    use HasFactory;
+
+    public $table = "blogs";
+    protected $fillable = [
+        'title',
+        'category',
+        'author',
+        'image',
+        'description',
+
+    ];
+    public $timestamp  = true;
+}
