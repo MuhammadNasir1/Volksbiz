f26fecd3acc114f14597c6b241c9ff69fbba963a
M-ArhamWaheed <info.epage.org@gmail.com>
2024-08-20 18:22:12 +0500

    Fix Bugs

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -41,6 +41,31 @@ class SubscriptionController extends Controller
             return redirect('subscriptionPlan');
         }
     }
+    public function update(Request $request, string $id)
+    {
+
+        try {
+
+            $validateData =    $request->validate([
+
+                'name' => 'required',
+                'price' => 'required',
+                'option' => 'required',
+                'plan_for' => 'required',
+            ]);
+            $optionsJson = json_encode($validateData['option']);
+
+            $subscribtion = Subscription::find($id);
+            $subscribtion->name = $validateData['name'];
+            $subscribtion->price = $validateData['price'];
+            $subscribtion->option = $optionsJson;
+            $subscribtion->price = $validateData['price'];
+            $subscribtion->update();
+            return response()->json(['success' => true, 'message' => 'Plan Update successfully'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()], 400);
+        }
+    }
 
     public function getSubscriptionData()
     {
@@ -52,6 +77,19 @@ class SubscriptionController extends Controller
                 $data->option = json_decode($data->option);
             }
             return response()->json(['success' => true, 'message' => "Data get successfully", 'data' => $subscriptionData], 200);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function editSubscriptionData(string $id)
+    {
+
+        try {
+
+            $subscriptionData = Subscription::all();
+            $editData = Subscription::find($id);
+            return view('subscription_plan', compact('subscriptionData', 'editData'));
         } catch (\Exception $e) {
             return response()->json(['success' => false, 'message' => $e->getMessage()], 500);
         }
--- resources/views/subscription_plan.blade.php
+++ resources/views/subscription_plan.blade.php
@@ -45,7 +45,7 @@
                                 <td>
                                     <div class="flex gap-5 items-center justify-center">
 
-                                        <a href=""><img width="38px"
+                                        <a href="../editSubscription/{{ $data->id }}"><img width="38px"
                                                 src="{{ asset('images/icons/edits.svg') }}" alt="update"></a>
                                         <a href="../deleteSubscription/{{ $data->id }}"> <img width="38px"
                                                 src="{{ asset('images/icons/delete.svg') }}" alt="Delete"></a>
@@ -68,107 +68,148 @@
 <div id="addcustomermodal" data-modal-backdrop="static"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0  left-0 z-50 justify-center  w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ">
     <div class="relative p-4 w-full  rounded-3xl  max-w-3xl max-h-full ">
-        <form action="../addSubscription" method="post" enctype="multipart/form-data">
-            @csrf
-            <div class="relative bg-white shadow-dark rounded-lg  dark:bg-gray-700  ">
-                <div class="flex items-center   justify-start  p-5  rounded-t dark:border-gray-600 bg-primary">
-                    <h3 class="text-xl font-semibold text-white ">
-                        @lang('lang.Add_Plan')
-                    </h3>
-                    <button type="button"
-                        class=" absolute right-2 text-white bg-transparent rounded-lg text-sm w-8 h-8 ms-auto "
-                        data-modal-hide="addcustomermodal">
+        @if (isset($editData))
+            <form id="subscriptionData" method="post" enctype="multipart/form-data"
+                url="../UpdateSubscription/{{ $editData->id }}">
+            @else
+                <form action="../addSubscription" method="post" enctype="multipart/form-data">
+        @endif
+        @csrf
+        <div class="relative bg-white shadow-dark rounded-lg  dark:bg-gray-700  ">
+            <div class="flex items-center   justify-start  p-5  rounded-t dark:border-gray-600 bg-primary">
+                <h3 class="text-xl font-semibold text-white ">
+                    @lang(!isset($editData) ? 'lang.Add_Plan' : 'lang.Update_Plan')
+                </h3>
+                <button type="button"
+                    class=" absolute right-2 text-white bg-transparent rounded-lg text-sm w-8 h-8 ms-auto "
+                    data-modal-hide="addcustomermodal">
+                    @if (isset($editData))
+                        @php
+                            $options = json_decode($editData->option);
+                        @endphp
+                        <a href="../subscriptionPlan">
+                            <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg></a>
+                    @else
                         <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                         </svg>
-                    </button>
+                    @endif
+                </button>
+            </div>
+
+
+            <div class="p-5">
+                <div class="">
+                    <div class="flex w-full lg:flex-row flex-col gap-5">
+                        <div class="w-full mt-4">
+                            <label class="text-[16px] font-semibold block  text-[#452C88]"
+                                for="plan_name">@lang('lang.Plan_Name')</label>
+                            <input type="text"
+                                class="w-full mt-2  border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary   h-[46px] text-[14px]"
+                                name="name" id="plan_name" placeholder="@lang('lang.Enter_Plan_Name')"
+                                value="{{ !isset($editData) ? '' : $editData->name }}">
+                        </div>
+                        <div class="w-full lg:mt-4">
+                            <label class="text-[16px] font-semibold block  text-[#452C88]"
+                                for="plan_price">@lang('lang.Plan_Price')</label>
+                            <input type="text"
+                                class="w-full mt-2  border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary   h-[46px] text-[14px]"
+                                name="price" id="plan_price" placeholder="@lang('lang.Plan_Price')"
+                                value="{{ !isset($editData) ? '' : $editData->price }}">
+                        </div>
+
+                    </div>
                 </div>
 
-
-                <div class="p-5">
-                    <div class="">
-                        <div class="flex w-full lg:flex-row flex-col gap-5">
-                            <div class="w-full mt-4">
-                                <label class="text-[16px] font-semibold block  text-[#452C88]"
-                                    for="plan_name">@lang('lang.Plan_Name')</label>
-                                <input type="text"
-                                    class="w-full mt-2  border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary   h-[46px] text-[14px]"
-                                    name="name" id="plan_name" placeholder="@lang('lang.Enter_Plan_Name')">
-                            </div>
-                            <div class="w-full lg:mt-4">
-                                <label class="text-[16px] font-semibold block  text-[#452C88]"
-                                    for="plan_price">@lang('lang.Plan_Price')</label>
-                                <input type="text"
-                                    class="w-full mt-2  border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary   h-[46px] text-[14px]"
-                                    name="price" id="plan_price" placeholder="@lang('lang.Plan_Price')">
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="w-full mt-2">
-                        <label class="text-[16px] font-semibold block  text-[#452C88]"
-                            for="plan_price">@lang('lang.Plan_For')</label>
-                        <select name="plan_for" id="plan_for">
-                            <option value="" selected disabled>@lang('lang.Select_For')</option>
+                <div class="w-full mt-2">
+                    <label class="text-[16px] font-semibold block  text-[#452C88]"
+                        for="plan_price">@lang('lang.Plan_For')</label>
+                    <select name="plan_for" id="plan_for">
+                        <option value="" selected disabled>@lang('lang.Select_For')</option>
+                        @if (isset($editData))
+                            <option value="buyer" {{ $editData->plan_for == 'buyer' ? 'selected' : '' }}>
+                                @lang('lang.Buyer')
+                            </option>
+                            <option value="seller" {{ $editData->plan_for == 'seller' ? 'selected' : '' }}>
+                                @lang('lang.Seller')
+                            </option>
+                        @else
                             <option value="buyer">@lang('lang.Buyer')</option>
                             <option value="seller">@lang('lang.Seller')</option>
-                        </select>
-                    </div>
-                    <div class="mt-2">
-                        <div class="flex w-full lg:flex-row flex-col gap-5">
-                            <div class="w-full mt-4">
-                                <label class="text-[16px] font-semibold block  text-[#452C88]"
-                                    for="plan_options">@lang('lang.Plan_Options')</label>
+                        @endif
 
-                                <div id="inputContainer"></div>
-                                <div class="flex gap-4">
-                                    <input type="text"
-                                        class="w-full mt-2 border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary h-[46px] text-[14px]"
-                                        name="option[]" id="plan_options" placeholder="@lang('lang.Enter_Plan_Options')">
-                                    <button type="button"
-                                        class="bg-primary text-white rounded-[4px] mt-2 px-4 py-2 addBtn font-semibold"
-                                        id="addOption">
-                                        +
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
+                    </select>
                 </div>
+                <div class="mt-2">
+                    <div class="flex w-full lg:flex-row flex-col gap-5">
+                        <div class="w-full mt-4">
+                            <label class="text-[16px] font-semibold block  text-[#452C88]"
+                                for="plan_options">@lang('lang.Plan_Options')</label>
+                            <div class="flex gap-4">
+                                <input type="text" value="{{ isset($options[0]) ? $options[0] : '' }}"
+                                    class="w-full mt-2 border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary h-[46px] text-[14px]"
+                                    name="option[]" id="plan_options" placeholder="@lang('lang.Enter_Plan_Options')">
+                                <button type="button"
+                                    class="bg-primary text-white rounded-[4px] mt-2 px-4 py-2 addBtn font-semibold"
+                                    id="addOption">
+                                    +
+                                </button>
+                            </div>
+                            <div id="inputContainer">
+                                @if (isset($options) && is_array($options))
+                                    @foreach (array_slice($options, 1) as $option)
+                                        <div class="flex gap-4 mt-2">
+                                            <input type="text" value="{{ $option }}" name="option[]"
+                                                class="w-full mt-2 border-2 border-[#DEE2E6] rounded-[6px] focus:border-primary h-[46px] text-[14px]" />
 
-
-
-
-
-
-
-                <div class="flex justify-end ">
-                    <button class="bg-primary text-white py-2 px-6 my-4 rounded-[4px]  mx-6 uaddBtn  font-semibold "
-                        id="addBtn">
-                        <div class=" text-center hidden" id="spinner">
-                            <svg aria-hidden="true"
-                                class="w-5 h-5 mx-auto text-center text-gray-200 animate-spin fill-primary"
-                                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                    fill="currentColor" />
-                                <path
-                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                    fill="currentFill" />
-                            </svg>
+                                            <button type="button"
+                                                class="text-white rounded-[4px] mt-2 px-3.5 py-1.5 deleteBtn font-semibold bg-red-900">
+                                                <i class="fa-solid fa-trash text-white"></i>
+                                            </button>
+                                        </div>
+                                    @endforeach
+                                @endif
+                            </div>
                         </div>
-                        <div id="text">
-                            @lang('lang.Save')
-                        </div>
 
-                    </button>
+                    </div>
                 </div>
             </div>
+
+
+
+
+
+
+
+            <div class="flex justify-end ">
+                <button class="bg-primary text-white py-2 px-6 my-4 rounded-[4px]  mx-6 uaddBtn  font-semibold "
+                    id="addBtn">
+                    <div class=" text-center hidden" id="spinner">
+                        <svg aria-hidden="true"
+                            class="w-5 h-5 mx-auto text-center text-gray-200 animate-spin fill-primary"
+                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor" />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill" />
+                        </svg>
+                    </div>
+                    <div id="text">
+                        @lang(!isset($editData) ? 'lang.Save' : 'lang.Update')
+                    </div>
+
+                </button>
+            </div>
+        </div>
         </form>
         <div>
 
@@ -182,6 +223,14 @@
 
 
 @include('layouts.footer')
+@if (isset($editData))
+    <script>
+        $(document).ready(function() {
+            $('#addcustomermodal').removeClass("hidden");
+            $('#addcustomermodal').addClass("flex");
+        });
+    </script>
+@endif
 <script>
     document.getElementById('addOption').addEventListener('click', () => {
         // Create a new input element
@@ -210,9 +259,54 @@
         // Append the container div to the main input container
         document.getElementById('inputContainer').appendChild(inputContainer);
 
+
         // Add an event listener to the delete button
         deleteButton.addEventListener('click', () => {
             inputContainer.remove();
+        });
+
+    });
+    document.querySelectorAll('.deleteBtn').forEach(button => {
+        button.addEventListener('click', function() {
+            this.parentElement.remove();
+        });
+    });
+    $(document).ready(function() {
+        $("#subscriptionData").submit(function(event) {
+            let url = $('#subscriptionData').attr('url');
+            event.preventDefault();
+
+            var formData = new FormData(this);
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: formData,
+                dataType: "json",
+                contentType: false,
+                processData: false,
+                beforeSend: function() {
+                    $('#spinner').removeClass('hidden');
+                    $('#text').addClass('hidden');
+                    $('#addBtn').attr('disabled', true);
+                },
+                success: function(response) {
+                    window.location.href = '../subscriptionPlan';
+
+                },
+                error: function(jqXHR) {
+                    let response = JSON.parse(jqXHR.responseText);
+                    console.log("error");
+                    Swal.fire(
+                        'Warning!',
+                        response.message,
+                        'warning'
+                    );
+
+                    $('#text').removeClass('hidden');
+                    $('#spinner').addClass('hidden');
+                    $('#addBtn').attr('disabled', false);
+                }
+            });
         });
     });
 </script>
