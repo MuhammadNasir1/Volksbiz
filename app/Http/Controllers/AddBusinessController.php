6d4d1738292576a999f8c4b7e25e8a34a787ce0e
MuhammadNasir1 <nmian7080@gmail.com>
2024-09-18 13:01:23 +0500

    v1

--- app/Http/Controllers/AddBusinessController.php
+++ app/Http/Controllers/AddBusinessController.php
@@ -276,8 +276,8 @@ class AddBusinessController extends Controller
         foreach ($orders as $order) {
             $businessId = $order['businuess_id'];
             $businesses = AddBusiness::where('id', $businessId)->first();
-            $businesses->images = json_decode($businesses->images);
             return response()->json($businesses);
+            $businesses->images = json_decode($businesses->images);
             $order->business = $businesses;
 
             $userId = $order['user_id'];
